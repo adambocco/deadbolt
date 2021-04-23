f1bc26fa0e25dafc5b1d5a43ee106be28e8d1575
Adam Bocco <adam.bocco@gmail.com>
2021-04-23 09:55:10 -0400

    Included files for creating html files and converting to arduino code

---
+++ htmlToArduino.py
@@ -0,0 +1,12 @@
+
+with open("index.html","r") as readFile, open("arduinoHtml","w") as writeFile:
+    for line in readFile:
+        i = 0
+        while i < len(line):
+            if line[i] == "\"" or line[i] == "'":
+                print(i)
+                line = line[:i] + "\\" + line[i:]
+                i+=1
+            i+=1
+        writeFile.write("ptr += \"" + line.rstrip() +"\"\n")
+        
