e6e2a217cfee99c6f16d96654ee3e34cd9473995
Adam Bocco <adam.bocco@gmail.com>
2021-05-01 19:41:25 -0400

    Added registration form validation

--- HttpBasicAuth/HttpBasicAuth.ino
+++ HttpBasicAuth/HttpBasicAuth.ino
@@ -120,14 +120,14 @@ void handleUnlock() {
 
 bool handlePinAuth() { 
   if (!server.hasArg("pin")){     //Parameter not found
-    server.send(400, "application/json", "{'response':'Enter a pin'}"); 
+    server.send(400, "application/json", "{\"response\":\"Enter a pin\"}"); 
   }
   else if (server.arg("pin") == PINCODE) {
-    server.send(200, "application/json", "{'response':'Unlocked'}");  
+    server.send(200, "application/json", "{\"response\":\"Unlocked\"}");  
     return true;
   } 
   else {
-    server.send(400, "application/json", "{'reponse': 'Incorrect pin'}");  
+    server.send(400, "application/json", "{\"reponse\": \"Incorrect pin\"}");  
   }
   return false; 
 }
