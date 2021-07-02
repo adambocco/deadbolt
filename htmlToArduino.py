700babd95faf2af2f6aadcd281ae91e9a55bb066
Adam Bocco <adam.bocco@gmail.com>
2021-07-02 01:36:08 -0400

    Hey

--- htmlToArduino.py
+++ htmlToArduino.py
@@ -1,10 +1,10 @@
 
-with open("index.html","r") as readFile, open("arduinoHtml","w") as writeFile:
+with open("waterfall.html","r") as readFile, open("arduinoHtml","w") as writeFile:
+    writeFile.write("char ptr[] = ")
     for line in readFile:
         i = 0
         while i < len(line):
             if line[i] == "\"" or line[i] == "'":
-                print(i)
                 line = line[:i] + "\\" + line[i:]
                 i+=1
             i+=1
